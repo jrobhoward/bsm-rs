3008e5d813576e665494e7b0fd61a91a590c7d96
James Howard <jhoward@thinkpad.jrh.net>
2017-11-17 22:14:39 -0600

    added byte order read, need to do a better job parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate ioctl_rs as ioctl;
+extern crate byteorder;
 
+use byteorder::{ByteOrder, LittleEndian, BigEndian, ReadBytesExt};
 use std::fs::File;
 use std::io::prelude::*;
 use std::os::unix::io::AsRawFd;
@@ -52,6 +54,12 @@ fn setup_serial_port(fd: RawFd) -> bool {
     true
 }
 
+const AUT_HEADER32: u8 = 0x14;
+const AUT_HEADER32_EX: u8 = 0x15;
+const AUT_HEADER64: u8 = 0x74;
+const AUT_HEADER64_EX: u8 = 0x79;
+const AUT_OTHER_FILE32: u8 = 0x11;
+
 
 fn main() {
     println!("Hello, world!");
@@ -65,9 +73,28 @@ fn main() {
 
     setup_serial_port(file.as_raw_fd());
 
-    let mut buffer = [0; 32767];
-    let result = file.read(&mut buffer);
-    for i in 0..result.unwrap() {
-        println!("Goodbye, world! {}", buffer[i]);
+    for _ in 0..5 {
+        let mut record_type: [u8; 1] = [0; 1];
+        let result = file.read(&mut record_type);
+        println!("Record Received type={}", record_type[0]);
+
+        let record_size = file.read_u32::<BigEndian>().unwrap();
+        println!("Record Received size={}", record_size);
+        let record_remaining = (record_size - 5) as usize;
+
+        let mut record_buffer: [u8; 32767] = [0; 32767];
+        let result = file.read(&mut record_buffer[0..record_remaining]);
+        println!("Read record numBytes={:?}", result);
+
+        println!("Four: {} {} {} {}", record_buffer[0], record_buffer[1], record_buffer[2], record_buffer[3]);
+
+        let token_size = BigEndian::read_u32(&record_buffer[0..5]);
+        println!("Token Received size={}", token_size);
+
+        for i in 0..record_remaining {
+        println!("  x[{}] = {}",i, record_buffer[i]);
+        }
+
     }
+
 }
